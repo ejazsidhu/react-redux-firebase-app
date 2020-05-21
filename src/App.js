df19cfd80d13478729706c9d1730c736765d775e
ejazsidhu <ejazsidhu@gmail.com>
2020-05-21 13:41:51 +0500

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-  <h1> Mario One</h1>
-  
-    </div>
+    <Router>
+      <div className="App">
+        <h1> Mario One</h1>
+
+      </div>
+    </Router>
   );
 }
 
