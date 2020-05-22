42a83198f31f696605c4cedb361adf74b3ee8820
ejazsidhu <ejazsidhu@gmail.com>
2020-05-22 14:40:51 +0500

    signup component added

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+
+class SignUp extends Component {
+    state = { 
+        email:'',
+        password:null
+     }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.id]:e.target.value
+        })
+
+    }
+    render() { 
+        return ( 
+            <div className="container">
+            <form onSubmit={this.handleSubmit} className="white">
+                <h5 className="grey-text text-darken-3">
+                    SignUp
+                </h5>
+                <div className="input-filed">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName" onChange={this.handleChange}/>
+                </div>
+                <div className="input-filed">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" onChange={this.handleChange}/>
+                </div>
+                <div className="input-filed">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" onChange={this.handleChange}/>
+                </div>
+                <div className="input-filed">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" onChange={this.handleChange}/>
+                </div>
+                <div className="input-filed">
+                    <button className="btn pink lighten-1 z-depth-0">
+                        SignUp
+                    </button>
+                </div>
+            </form>
+            </div>
+         );
+    }
+}
+ 
+export default SignUp;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as Navbar} from "./layouts/Navbar";
 export {default as Dashboard} from "./dashboard/Dashboard";
 export {default as ProjectDetails} from "./projects/ProjectDetail"
 export {default as SignIn} from "./auth/SignIn"
+export {default as SignUp} from "./auth/SignUp"
