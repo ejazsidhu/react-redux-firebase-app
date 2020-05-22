508bcc071fbc362886c405fb5790e6e3303d4364
ejazsidhu <ejazsidhu@gmail.com>
2020-05-22 18:13:22 +0500

    logo added

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import  SignedInLinks  from "./SignedInLinks";
-import SignedOutLinks from "./SingedOutLinks"
+import SignedOutLinks from "./SingedOutLinks";
+import mario from '../../img/mario-logo.png'
 
 
 const Navbar = () => {
+
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <Link to="/" className="brand-logo">MarioPlan</Link>
+                <Link to="/" className="brand-logo">
+                    <img className="m1" style={{margin:5}} src={mario} width="45" alt="" srcset=""/>
+                </Link>
               <SignedInLinks/>
               <SignedOutLinks/>
             </div>
